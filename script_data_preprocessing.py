a3cc53c89a64e09637b70108953cdf7abd9a180f
dee6600 <durgeshreddiyar@yahoo.com>
2019-06-22 09:37:53 +0530

    Edited code

--- script_data_preprocessing.py
+++ script_data_preprocessing.py
@@ -3,7 +3,7 @@ import glob
 import ntpath
 
 video_paths = (glob.glob("/home/durgesh/PycharmProjects/video_to_frame_script/videos_input/*.avi"))
-#print(video_paths)
+
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
@@ -12,9 +12,7 @@ def path_leaf(path):
 j=0
 for i in video_paths:
     name_of_video = path_leaf(i)
-   # print(name_of_video)
 
-    #print("ffmpeg -i %s -vf fps=1 %s%s/thumb%%04d.jpg -hide_banner" % (video_paths[j],"/home/durgesh/PycharmProjects/video_to_frame_script/script_output/",name_of_video))
     name_of_folder= name_of_video.split('.')
     os.system("mkdir /home/durgesh/PycharmProjects/video_to_frame_script/script_output/%s" %(name_of_folder[0]))
     os.system("ffmpeg -i %s -vf fps=1 %s%s/thumb%%04d.jpg -hide_banner" % (video_paths[j], "/home/durgesh/PycharmProjects/video_to_frame_script/script_output/", name_of_folder[0]))
